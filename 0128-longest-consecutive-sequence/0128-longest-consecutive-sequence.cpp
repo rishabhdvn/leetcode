cc66c15abdd282157c877ec4a9936c8d8d5c10f9
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-04-28 18:32:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int maxCount=1;
+        int count=1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]!=nums[i+1]){
+                if(nums[i+1]-nums[i]==1){
+                count++;
+            }
+            else{
+                maxCount=max(maxCount,count);
+                count=1;
+            }
+        }
+        }
+            
+        return max(maxCount,count);
+    }
+};
