a002cceee1837bdd63b749bb719c459af26dcfb3
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-06-30 14:45:19 +0530

    Time: 7 ms (20.73%), Space: 23.6 MB (18.3%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left=0;
+        int right=s.size()-1;
+        string temp="";
+        string res="";
+        while(left<=right){
+        char ch= s[left];
+        if(ch !=' '){
+            temp+=ch;
+        }
+        else if(!temp.empty()){
+            if(!res.empty()){
+                res=temp+" "+res;
+            }
+            else{
+                res=temp;
+            }
+            temp="";
+        }
+        left++;
+        }
+        if(!temp.empty()){
+            if(!res.empty()){
+                res=temp+" "+res;
+            }
+            else{
+                res=temp;
+            }
+    }
+        return res; 
+    }
+};
