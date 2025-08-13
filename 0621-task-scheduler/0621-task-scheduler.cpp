699f0c8137a3e55a3b583ad20130fea2d5ee147a
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-08-13 15:24:38 +0530

    Time: 26 ms (48.05%), Space: 38.6 MB (62.19%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char,int> freq;
-        for( char ch: tasks){
+        for(char ch : tasks){
             freq[ch]++;
         }
         priority_queue<pair<int,char>> maxHeap;
@@ -11,25 +11,23 @@ public:
         }
         using Node=tuple<int,int,char>;
         priority_queue<Node,vector<Node>,greater<Node>> minHeap;
-
         int time=0;
         while(!maxHeap.empty() || !minHeap.empty()){
             time++;
             if(!maxHeap.empty()){
-                auto[count,ch]=maxHeap.top();
-                maxHeap.pop();
-                count--;
-                if(count>0){
-                    minHeap.push({time+n,count,ch});
-                }
-            }
-            if(!minHeap.empty() && get<0>(minHeap.top())==time){
-                auto[ready,count,ch]=minHeap.top();
-                minHeap.pop();
-                maxHeap.push({count,ch});
+            auto[count,ch]=maxHeap.top();
+            maxHeap.pop();
+            count--;
+            if(count>0){
+                minHeap.push({time+n,count,ch});
             }
         }
+        while(!minHeap.empty() && get<0>(minHeap.top())==time){
+            auto[readyTime,count,ch]=minHeap.top();
+            minHeap.pop();
+            maxHeap.push({count,ch});
+        }
+        }
         return time;
-        
     }
 };
