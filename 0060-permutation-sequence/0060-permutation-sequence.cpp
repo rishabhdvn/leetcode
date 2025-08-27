d0369457796fb96ad14b3e2df77d61bfb561827e
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-08-27 16:44:20 +0530

    Time: 471 ms (8.71%), Space: 8.7 MB (55.3%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    string ans = "";
-    int count = 0;   
-
-    void solve(int n, int k, vector<string>& arr, vector<bool>& used, vector<string>& current) {
-        if (current.size() == n) {
-            count++;
-            if (count == k) {
-                for (string& s : current) ans += s;
+string ans="";
+int count=0;
+void solve(int n,int k,vector<string>& arr,vector<bool>& used,vector<string>& current){
+    if(current.size()==n){
+        count++;
+        if(count==k){
+            for(string& s : current){
+                ans+=s;
             }
-            return;
         }
-        for (int i = 1; i <= n; i++) {  
-            if (!used[i]) {
-                used[i] = true;
+           return ;
+    }
+        for(int i=1;i<=n;i++){
+            if(!used[i]){
+                used[i]=true;
                 current.push_back(arr[i]);
-                solve(n, k, arr, used, current);
-                if (!ans.empty()) return; 
+                solve(n,k,arr,used,current);
+                if(!ans.empty()) return;
                 current.pop_back();
-                used[i] = false;
+                used[i]=false;
             }
         }
     }
 
     string getPermutation(int n, int k) {
-        vector<string> arr = {"","1","2","3","4","5","6","7","8","9"};
-        vector<bool> used(n+1, false);
+        vector<string> arr={"","1","2","3","4","5","6","7","8","9"};
+        vector<bool> used(n+1,false);
         vector<string> current;
-        solve(n, k, arr, used, current);
-        return ans;
+        solve(n,k,arr,used,current);
+        return ans; 
     }
 };
