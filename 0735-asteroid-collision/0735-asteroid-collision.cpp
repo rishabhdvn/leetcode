b02538a38ed0274f3ebe093b9c196aa741e099ac
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-09-01 16:30:51 +0530

    Time: 7 ms (6.05%), Space: 21.7 MB (80.61%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> ans;
-        for (int i = 0; i < (int)asteroids.size(); i++) {
-            int current = asteroids[i];
-            bool destroyed = false;
+    for(int i=0;i<(int)asteroids.size();i++){
+        int current=asteroids[i];
+        bool destroyed=false;
+        while(!ans.empty() && current < 0 && ans.back()>0){
+       if(ans.back()<-current){
+        ans.pop_back();
+        continue;
+       }
+       else if(ans.back()==-current){
+        ans.pop_back();
+        destroyed=true;
+        break;
+       }
+       else{
+        destroyed=true;
+        break;
+       }
 
-            // collide only when stack top is moving right and current moves left
-            while (!ans.empty() && current < 0 && ans.back() > 0) {
-                if (ans.back() < -current) {
-                    // stack asteroid smaller -> it explodes; keep checking further
-                    ans.pop_back();
-                    continue;
-                } else if (ans.back() == -current) {
-                    // equal size -> both explode
-                    ans.pop_back();
-                    destroyed = true;
-                    break;
-                } else { 
-                    // stack asteroid larger -> current explodes
-                    destroyed = true;
-                    break;
-                }
-            }
-
-            if (!destroyed) ans.push_back(current);
         }
-        return ans;
+          if(!destroyed) ans.push_back(current);
+    }
+    return ans;
+        
     }
 };
