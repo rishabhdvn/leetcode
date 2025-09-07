211c065fc5793cb012d2be249a2c9781342194d8
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-09-07 14:57:56 +0530

    Time: 281 ms (43.71%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,5 @@
-With Commontable AS
-(SELECT * ,DENSE_RANK() OVER(ORDER BY salary DESC) AS r
-FROM Employee)
-
-SELECT IFNULL((SELECT salary  FROM CommonTable WHERE r=2 LIMIT 1),null) 
-as  SecondHighestSalary
+SELECT 
+    (SELECT MAX(salary) 
+     FROM Employee 
+     WHERE salary < (SELECT MAX(salary) FROM Employee)
+    ) AS SecondHighestSalary;
