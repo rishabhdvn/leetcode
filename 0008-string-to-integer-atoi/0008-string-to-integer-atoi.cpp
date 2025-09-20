13220903c0b97b682414814386a17819a8085a41
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-09-20 16:31:57 +0530

    Time: 5 ms (3.41%), Space: 11.3 MB (5.45%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
+int parseDigits(string s,int index,long long num,int sign){
+    int n=s.size();
+    if(index>=n || !isdigit(s[index])) return (int)(num*sign);
+    if(num==0 && s[index]=='0'){
+        return parseDigits(s,index+1,0,sign);
+    }
+    num=num*10+(s[index]-'0');
+    if(sign==1 && num>INT_MAX) return INT_MAX;
+    if(sign==-1 && -num<INT_MIN) return INT_MIN;
+   return parseDigits(s,index+1,num,sign);
+}
     int myAtoi(string s) {
-        int i=0,n=s.size();
-        while(i<n && s[i]==' ') i++;
-         
-         int sign=1;
-         if(i<n && (s[i]=='+' || s[i]=='-')){
-            sign=(s[i]=='-')? -1:1;
-            i++;
-         }
-         long long res=0;
-         while(i<n && isdigit(s[i])){
-            res=res*10+(s[i]-'0');
-            if(res*sign>=INT_MAX) return INT_MAX;
-            if(res*sign<=INT_MIN) return INT_MIN;
-            i++;
-         }
-         return res*sign;  
+        int n=s.size();
+        int index=0;
+        while(index<n && s[index]==' ')index++;
+        int sign=1;
+        if(index<n && (s[index]=='+' || s[index]=='-')){
+            if(s[index]=='-') sign=-1;
+            index++;
+        }
+        return parseDigits(s,index,0,sign);
+        
     }
 };
