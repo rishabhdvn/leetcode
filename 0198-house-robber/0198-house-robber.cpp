8a20c837c6f0ffa11e95d5c05f338cbefca27929
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-10-26 16:50:28 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+int robRecursive(vector<int>& nums,int index,vector<int>& dp){
+    if(index>=nums.size()) return 0;
+    if(dp[index]!=-1) return dp[index];
+    int take=nums[index]+robRecursive(nums,index+2,dp);
+    int notTake=robRecursive(nums,index+1,dp);
+    dp[index]=max(take,notTake);
+    return dp[index];
+}
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        vector<int> dp(n);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-        }
-        return dp[n-1];      
+        vector<int> dp(nums.size(),-1);
+        return robRecursive(nums,0,dp);
+        
     }
 };
