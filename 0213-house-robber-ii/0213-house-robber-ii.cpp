3612e86e5aa143c29bc7f6fd4be6416feae879c6
rishabhdvn <rishabhdhawan2003@gmail.com>
2025-10-26 17:14:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    
 public:
-int rob2(vector<int>&nums,int start,int end){
-    int n=end-start+1;
-    if(n==1) return nums[start];
-    vector<int> dp(n);
-    dp[0]=nums[start];
-    dp[1]=max(nums[start],nums[start+1]);
-    for(int i=2;i<n;i++){
-        dp[i]=max(dp[i-1],dp[i-2]+nums[start+i]);
-    }
-    return dp[n-1];
+int robRecursively(vector<int>& nums,int index){
+    if(index>=nums.size()) return 0;
+    int take=nums[index]+robRecursively(nums,index+2);
+    int notTake=robRecursively(nums,index+1);
+
+    return max(take,notTake);
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==0) return 0;
         if(n==1) return nums[0];
-        return max(rob2(nums,0,n-2),rob2(nums,1,n-1));     
+        vector<int> firstScenario(nums.begin(),nums.end()-1);
+        int rob1=robRecursively(firstScenario,0);
+        vector<int> secondScenario(nums.begin()+1,nums.end());
+        int rob2=robRecursively(secondScenario,0);
+        return max(rob1,rob2);
     }
 };
